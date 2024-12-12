b84d795a631e3d5b16508ffb7143a3e18a72b95f
khacduc204 <khacduc246@gmail.com>
2024-12-12 12:56:50 +0700

    Harmic

---
+++ Untilities/Function.cs
@@ -0,0 +1,11 @@
+﻿namespace Harmic.Untilities
+{
+    public class Function
+    {
+        // Tạo alias slug từ title
+        public static string TitleSlugGenerationAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
---
+++ ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.ViewComponents
+{
+	public class BlogViewComponent : ViewComponent
+	{
+		private readonly HarmicContext _context;
+
+		public BlogViewComponent(HarmicContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = _context.TbBlogs
+				.Include(m => m.Category)
+				.Where(b => (bool)b.IsActive) 
+				.OrderByDescending(b => b.BlogId)
+				.ToList();
+
+			return await Task.FromResult<IViewComponentResult>(View(items));
+		}
+	}
+}
